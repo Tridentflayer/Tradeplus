96811a00351c0535f40281d98ee47490716e169a
Tridentflayer <eltrancy@gmail.com>
2021-01-08 18:12:29 -0500

    Update ModItems.java

--- src/main/java/tridentshort/tradeplus/tradeplus/ModItems.java
+++ src/main/java/tridentshort/tradeplus/tradeplus/ModItems.java
@@ -8,16 +8,16 @@ import net.minecraft.util.registry.Registry;
 public class ModItems
 {
 
-    public static Item testItem;
+    public static Item testItem; //Item variable
 
 
     public static void init()
     {
-        testItem = registerItem("test_item", new Item(new Item.Settings().group(ItemGroup.MISC)));
+        testItem = registerItem("test_item", new Item(new Item.Settings().group(ItemGroup.MISC))); //Copy this line and change testItem to the item variable name and name to item name
     }
 
 
-    public static Item registerItem(String name, Item item)
+    public static Item registerItem(String name, Item item) //No touch
     {
         return Registry.register(Registry.ITEM, new Identifier("tradeplus", name), item);
     }
