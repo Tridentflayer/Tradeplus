5f4c3d614d3d6c51bf3e95bba7eed5fca4062bc8
Tridentflayer <eltrancy@gmail.com>
2021-01-09 18:38:07 -0500

    Update ModTradeBuilder.java

--- src/main/java/tridentshort/tradeplus/tradeplus/ModTradeBuilder.java
+++ src/main/java/tridentshort/tradeplus/tradeplus/ModTradeBuilder.java
@@ -16,11 +16,14 @@ public class ModTradeBuilder
         TradeOffers.PROFESSION_TO_LEVELED_TRADE.put(ModVillagerProfessions.BeeKeeper, copyToFastUtilMap(ImmutableMap.of(
                 1, new TradeOffers.Factory[] //Number (in this case, 1) represents villager levels from lowest to highest.
                         {
-                                new TradeOfferFactories.BuyForOneEmeraldFactory(Items.HONEYCOMB, 1, 10, 10),
-                                new TradeOfferFactories.BuyForOneEmeraldFactory(Blocks.BEE_NEST, 4, 4, 10)    //Trades in each level
+                                new TradeOfferFactories.BuyForOneEmeraldFactory(Items.HONEYCOMB, 2, 12, 1), //Trades in each level. If there's more than 2 per level it will randomly choose two
+                                new TradeOfferFactories.BuyForOneEmeraldFactory(Items.HONEY_BOTTLE, 4, 16, 2),
                         },
                 2, new TradeOffers.Factory[]
                         {
+                                new TradeOfferFactories.SellItemFactory(Blocks.BEEHIVE, 6, 12, 5),
+                                new TradeOfferFactories.SellItemFactory(Blocks.HONEY_BLOCK, 2, 12, 2),
+                                new TradeOfferFactories.SellItemFactory(Blocks.HONEYCOMB_BLOCK, 2, 12, 2),
 
                         },
                 3, new TradeOffers.Factory[]
